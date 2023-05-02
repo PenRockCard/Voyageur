124a1a15d41be0a2086bb26a72ddc225db1a0abc
PenRockCard <PRC@Desktop>
2023-05-01 22:04:56 -0400

    Increased number of orbitalCharacteristicPoints to allow the outer system to update properly. Also removed Pluto's orbital offset and angle to make it easier to update properly

--- Voyageur/Game/MainGame.cpp
+++ Voyageur/Game/MainGame.cpp
@@ -11,7 +11,7 @@ MainGame::MainGame() {
     timePerTick=1000;
 
     //TODO: Add sun distance and angle offset to parameters
-    int orbitalCharacteristicPoints = 9999; //Neptune stops working when it's set to 10,000 for some reason.
+    int orbitalCharacteristicPoints = 100000; //Neptune stops working when it's set to 10,000 for some reason.
     //Planet masses are based off of Earth's (from Wikipedia)
     double earthMass = 5972200000000000000000000.0; //Mass in kg. It has to have a .0 at the end to define it as a float/double, too big for an int/long
     planets.push_back(new Planet("Mercury", &people, nextPlanetID++, resourceTemplates, 0.466697*AU, 0.307499*AU, 360, orbitalCharacteristicPoints, earthMass*0.0553, &timePerTick));
@@ -22,9 +22,9 @@ MainGame::MainGame() {
     planets.push_back(new Planet("Saturn", &people, nextPlanetID++, resourceTemplates, 10.1238*AU,9.0412*AU, 360, orbitalCharacteristicPoints, earthMass*95.2, &timePerTick));
     planets.push_back(new Planet("Uranus", &people, nextPlanetID++, resourceTemplates, 20.0965*AU,18.2861*AU, 360, orbitalCharacteristicPoints, earthMass*14.6, &timePerTick));
     planets.push_back(new Planet("Neptune", &people, nextPlanetID++, resourceTemplates, 30.33*AU,29.81*AU,  360, orbitalCharacteristicPoints, earthMass*17.2, &timePerTick));
-    planets.push_back(new Planet("Pluto", &people, nextPlanetID++, resourceTemplates, 49.305*AU,29.658*AU,  360, orbitalCharacteristicPoints, earthMass*0.0022, &timePerTick, 0.1*1000000000,0.5*1000000000, 315));
+    planets.push_back(new Planet("Pluto", &people, nextPlanetID++, resourceTemplates, 49.305*AU,29.658*AU,  360, orbitalCharacteristicPoints, earthMass*0.0022, &timePerTick/*, 0.1*1000000000,0.5*1000000000, 315*/));
 
-    timePerTick=20000;
+    timePerTick=50000;
 
     updateTime = new UpdateTime();
 }
